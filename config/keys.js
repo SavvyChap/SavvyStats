6c52f6700cad543e6a463659b60be55e37451c28
SavvyChap <augustmoney81@gmail.com>
2018-09-30 17:19:05 -0500

    Initial app setup with Google authentication.

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+  cookieKey: 'uiwerahgiprwe7486243b2tqlbg789hg9wab7893f4',
+  googleClientID: '920258031805-ijhnlt6gvlmr282ivok0ocb6ol09p135.apps.googleusercontent.com',
+  googleClientSecret: '99CRgzwjCsS8g78US4uOs4i8',
+  mongoURI: 'mongodb://plex:Leggomyegg0@ds153380.mlab.com:53380/savvy-stats-01'
+};
